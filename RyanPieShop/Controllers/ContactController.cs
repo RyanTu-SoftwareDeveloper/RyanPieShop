e86c101a0b5d7f1d203e93b815c1bbb97edb2804
RyanTu-SoftwareDeveloper <ryantu89.work@gmail.com>
2024-04-29 17:08:40 -0700

    finished updating the views in the app

---
+++ RyanPieShop/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RyanPieShop.Controllers
+{
+    public class ContactController : Controller
+    {
+        public  IActionResult Index()
+        {
+            return View();  
+        }
+    }
+}
